203d0435294b06d842499db7f82b2ad843f0b8b4
Pawel Dudek <pawel@dudek.mobi>
2015-04-19 18:42:52 +0200

    Initial commit

---
+++ PBDNotificationObserver/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PBDNotificationObserver
+//
+//  Created by Paweł Dudek on 19/04/15.
+//  Copyright (c) 2015 Paweł Dudek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PBDNotificationObserver/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PBDNotificationObserver
+//
+//  Created by Paweł Dudek on 19/04/15.
+//  Copyright (c) 2015 Paweł Dudek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
